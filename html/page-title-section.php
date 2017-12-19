fee928e67df3257a0153df06b54e21b4f61161a9
Peter Selby <ps664@cornell.edu>
2017-12-19 12:24:42 -0500

    new brapps page

--- html/page-title-section.php
+++ html/page-title-section.php
@@ -1,7 +1,7 @@
 <section class="module-small bg-dark-60 about-page-header" data-background="assets/images/header.jpg">
     <div class="container">
       <div class="row">
-        <div class="col-sm-6 col-sm-offset-3">
+        <div class="col-sm-8 col-sm-offset-2">
           <h1 class="brapi-page-title font-alt"><?= isset($PageTitle) ? $PageTitle : "Default Title"?></h1>
         </div>
       </div>
