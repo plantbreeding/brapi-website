2cfc1d88f4d9fc6e06f6c90e38061e0731d57473
Peter Selby <ps664@cornell.edu>
2020-05-25 10:19:57 -0400

    get started page

---
+++ site/get-started.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en-US" dir="ltr">
+
+<?php
+    include("html/header.html");
+?>
+    <body data-spy="scroll" data-target=".onpage-navigation" data-offset="60">
+        <main>
+            <div class="page-loader">
+                <div class="loader">Loading...</div>
+            </div>
+            <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+                <?php
+                    include("html/menu.html");
+                ?>
+            </nav>
+            <?php
+                $PageTitle = "Get Started";
+                include("html/page-title-section.php");
+            ?>
+            <div class="main">
+                <?php
+                    include("html/get-started.html");
+                    include("html/footer.html");
+                ?>
+            </div>
+            <div class="scroll-up"><a href="#totop"><i class="mdi mdi-chevron-up"></i></a></div>
+        </main>        
+    </body>
+</html>
