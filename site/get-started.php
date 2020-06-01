2d6201d481e6f4b44c90a1ec9bf1c3acde4721f6
Peter Selby <ps664@cornell.edu>
2020-06-01 17:48:17 -0400

    update about

--- site/get-started.php
+++ site/get-started.php
@@ -25,6 +25,41 @@
                 ?>
             </div>
             <div class="scroll-up"><a href="#totop"><i class="mdi mdi-chevron-up"></i></a></div>
-        </main>        
+        </main>  
+        <script>
+            
+            $( document ).ready(function() {
+                $.getJSON("json/get-started-pages.json?nocache=" + Date.now(), function(pages){
+                    $.urlParam = function(name){
+                        var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+                        if(results){
+                            return results[1] || 0;
+                        }else{
+                            return undefined;
+                        }
+                    }
+
+                    var id = '0';
+                    if($.urlParam('id')){
+                        id = $.urlParam('id')
+                    }
+                    
+                    var content_template = $('#gs-content-template').html();
+                    var content = Mustache.render(content_template, $(pages)[0][id]);
+                    $('#content').append(content);
+
+                    var menu_template = $('#gs-menu-template').html();
+                    Mustache.parse(menu_template); 
+
+                    for (var key in $(pages)[0]) {
+                        var page = $(pages)[0][key];
+                        page.id = key;
+                        var rendered = Mustache.render(menu_template, page);
+                        $('#menu').append(rendered);
+                    }
+                    
+                });
+            });
+        </script>      
     </body>
 </html>
