ff29cfecf84962d1743c524b418ce90cf1deda0e
Peter Selby <ps664@cornell.edu>
2020-06-08 15:38:07 -0400

    update server config

--- server/init-letsencrypt.sh
+++ server/init-letsencrypt.sh
@@ -7,7 +7,7 @@ fi
 
 domains=(brapi.org www.brapi.org)
 rsa_key_size=4096
-data_path="./data/certbot"
+data_path="/home/selbyp/brapi.org/data/server-conf/certbot"
 email="brapicoordinatorselby@gmail.com" # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
