4e66fca23247f6d410e8d81eadd5882161976655
Peter Selby <ps664@cornell.edu>
2021-02-23 18:34:52 -0500

    login page created

---
+++ brapi-website-node/routes/oauth.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+
+const { Issuer, generators } = require('openid-client');
+var client;
+
+function buildOauthClient() {
+    return Issuer.discover('https://test-server.brapi.org/brapi/auth/') // => Promise
+        .then(function(issuerResponse) {
+            console.log('Discovered issuer %s %O', issuerResponse.issuer, issuerResponse.authorization_endpoint);
+
+            client = new issuerResponse.Client({
+                client_id: 'exampleClient',
+                redirect_uris: ['http://localhost:3000/oauth/redirect'],
+                //response_types: ['code']
+                response_types: ['token'],
+            }); // => Client
+        });
+}
+
+router.get('/', function(req, res, next) {
+    buildOauthClient().then(function() {
+        res.render('oauth', {
+            title: 'OAuth',
+            footerEvents: require('./events').getTrailerEvents()
+        });
+    });
+});
+
+router.get('/login', function(req, res, next) {
+    const nonce = generators.nonce();
+    var authURL = client.authorizationUrl({
+        scope: 'profile',
+    });
+    res.redirect(authURL);
+});
+
+
+router.get('/redirect', function(req, res, next) {
+    buildOauthClient().then(function() {
+        res.render('oauth', {
+            title: 'OAuth',
+            footerEvents: require('./events').getTrailerEvents()
+        });
+    });
+});
+module.exports = router;
