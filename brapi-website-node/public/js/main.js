031d0db427ec52bc55f67f0149a40aa33b8ab312
Peter Selby <ps664@cornell.edu>
2021-09-01 14:54:40 -0400

    virt hackathon updates

--- brapi-website-node/public/js/main.js
+++ brapi-website-node/public/js/main.js
@@ -1,14 +1,59 @@
-function toggleVisible(toggleId, btnId, displayTxt, hideTxt) {
-    var toggleElm = document.getElementById(toggleId);
-    var btnElm = document.getElementById(btnId);
-    if (toggleElm.style.display === "block") {
-        toggleElm.style.display = "none";
-        btnElm.innerText = hideTxt;
+function setVisible(elemId, isVis) {
+    var elem = document.getElementById(elemId);
+    if (isVis) {
+        elem.style.display = "block";
     } else {
-        toggleElm.style.display = "block";
-        btnElm.innerText = displayTxt;
+        elem.style.display = "none";
     }
 }
+
+function toggleVisible(toggleId, btnId, displayTxt, hideTxt) {
+    var toggleElm = document.getElementById(toggleId);
+    setVisible(toggleId, !(toggleElm.style.display === "block"));
+
+    var btnElm = document.getElementById(btnId);
+    if (toggleElm.style.display === "block")
+        btnElm.innerText = displayTxt;
+    else
+        btnElm.innerText = hideTxt;
+}
+
+function toggleVisibleXor(visibleId, invisibleIds) {
+    setVisible(visibleId, true);
+    window.location.hash = '#' + visibleId;
+    for (const id of invisibleIds) {
+        setVisible(id, false);
+    }
+    window.scrollTo(0, 0);
+}
+
+$(document).ready(function() {
+    console.log("document loaded");
+    hackathonLoadPage();
+});
+
+function hackathonLoadPage() {
+    page = window.location.hash.replace("#", "");
+    switch (page) {
+        case 'agenda-section':
+            toggleVisibleXor('agenda-section', ['overview-section', 'attendees-section', 'projects-section', 'notes-section']);
+            break;
+        case 'attendees-section':
+            toggleVisibleXor('attendees-section', ['overview-section', 'agenda-section', 'projects-section', 'notes-section']);
+            break;
+        case 'projects-section':
+            toggleVisibleXor('projects-section', ['overview-section', 'agenda-section', 'attendees-section', 'notes-section']);
+            break;
+        case 'notes-section':
+            toggleVisibleXor('notes-section', ['overview-section', 'agenda-section', 'attendees-section', 'projects-section']);
+            break;
+        case 'overview-section':
+        default:
+            toggleVisibleXor('overview-section', ['agenda-section', 'attendees-section', 'projects-section', 'notes-section']);
+    }
+
+}
+
 
 (function() {
 
