d44717deeb7b9770aef4976e9bbcb40f9dfc1d8b
Peter Selby <ps664@cornell.edu>
2023-08-08 17:14:46 -0400

    new logo image

--- brapi-website-node/routes/api.js
+++ brapi-website-node/routes/api.js
@@ -5,7 +5,32 @@ var nodemailer = require('nodemailer');
 var fs = require('fs');
 const fetch = require('node-fetch');
 
+const formData = require('form-data');
+const Mailgun = require('mailgun.js');
+const mailgun = new Mailgun(formData);
+const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY});
+
 router.post('/mailingListSubscribe', function(req, res, next) {
+    const now = new Date(Date.now());
+    var emailData = {
+        from: 'BrAPI Announcements <announcement.test@mail.brapi.org>',
+        to: 'announcement.test@mail.brapi.org',
+        subject: 'Important: BrAPI Mailing List Update - Message 2 of 2',
+        template: 'announcement_template',
+        text: 'We are changing to a new mailing server for the BrAPI mailing list. You should receive two messages today: (1) A message from the old server address \"<plant-breeding-api-bounces@mail2.sgn.cornell.edu>\" and (2) THIS message from the new server address \"announcements@mail.brapi.org\". \nCongratulations! If you are reading this, then your address has been successfully migrated to the new system, and your spam filter has been configured correctly to allow messages from \"@mail.brapi.org\". \nThis new mail server should provide more stability and features, as well as making it easier for me to manage news posts and announcements. Moving to the community emails \"mail.brapi.org\" domain will hopefully future-proof the system, so even if the mail server needs to change again, the address will not. Thank you for your patience and cooperation as we make these adjustments. \nIf you do not want to be included on the BrAPI mailing list anymore, you can Unsubscribe here: https://brapi.org/unsubscribe \nCheers \nPeter Selby',
+        'h:X-Mailgun-Variables': JSON.stringify({
+            "title": "Important: BrAPI Mailing List Update - Message 2 of 2",
+            "date": "August 19, 2022",
+            "author": "Peter Selby",
+            "article": "<p>We are changing to a new mailing server for the BrAPI mailing list. You should receive two messages today: (1) A message from the old server address \"<plant-breeding-api-bounces@mail2.sgn.cornell.edu>\" and (2) THIS message from the new server address \"announcements@mail.brapi.org\". </p><p>Congratulations! If you are reading this, then your address has been successfully migrated to the new system, and your spam filter has been configured correctly to allow messages from \"@mail.brapi.org\".</p><p>This new mail server should provide more stability and features, as well as making it easier for me to manage news posts and announcements. Moving to the community emails \"mail.brapi.org\" domain will hopefully future-proof the system, so even if the mail server needs to change again, the address will not. Thank you for your patience and cooperation as we make these adjustments. </p><p>If you do not want to be included on the BrAPI mailing list anymore, you can <a href=\"https://brapi.org/unsubscribe\">Unsubscribe</a> here: <a href=\"https://brapi.org/unsubscribe\">https://brapi.org/unsubscribe</a></p><p>Cheers<br>Peter Selby</p>"
+        }),
+        'h:List-Unsubscribe': 'http://brapi.org/unsubscribe',
+        'h:Date': now.toUTCString()
+    }
+
+    mg.messages.create('mail.brapi.org', emailData)
+    .then(msg => console.log(msg)) 
+    .catch(err => console.error(err));
     fs.appendFile(process.env.MIALING_LIST_PATH, '+ ' + req.body.name + ' (' + req.body.org + ')<' + req.body.email + '>\n', function(err) {
         if (err) throw err;
         res.json({ "success": "true" })
