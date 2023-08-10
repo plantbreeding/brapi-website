ca7e1818978ab0c074d974be87daaebdd7fcfb2b
Peter Selby <ps664@cornell.edu>
2023-08-10 12:04:14 -0400

    mail update

--- brapi-website-node/routes/api.js
+++ brapi-website-node/routes/api.js
@@ -8,43 +8,82 @@ const fetch = require('node-fetch');
 const formData = require('form-data');
 const Mailgun = require('mailgun.js');
 const mailgun = new Mailgun(formData);
-const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY});
+const mg = mailgun.client({ username: 'api', key: process.env.MAILGUN_API_KEY });
 
-router.post('/mailingListSubscribe', function(req, res, next) {
+router.post('/mailingListSubscribe', function (req, res, next) {
     const now = new Date(Date.now());
     var emailData = {
-        from: 'BrAPI Announcements <announcement.test@mail.brapi.org>',
-        to: 'announcement.test@mail.brapi.org',
-        subject: 'Important: BrAPI Mailing List Update - Message 2 of 2',
+        from: 'BrAPI Mailing List <mail@mail.brapi.org>',
+        to: [req.body.email, 'mail@mail.brapi.org'],
+        subject: 'Welcome to the BrAPI Mailing List',
         template: 'announcement_template',
-        text: 'We are changing to a new mailing server for the BrAPI mailing list. You should receive two messages today: (1) A message from the old server address \"<plant-breeding-api-bounces@mail2.sgn.cornell.edu>\" and (2) THIS message from the new server address \"announcements@mail.brapi.org\". \nCongratulations! If you are reading this, then your address has been successfully migrated to the new system, and your spam filter has been configured correctly to allow messages from \"@mail.brapi.org\". \nThis new mail server should provide more stability and features, as well as making it easier for me to manage news posts and announcements. Moving to the community emails \"mail.brapi.org\" domain will hopefully future-proof the system, so even if the mail server needs to change again, the address will not. Thank you for your patience and cooperation as we make these adjustments. \nIf you do not want to be included on the BrAPI mailing list anymore, you can Unsubscribe here: https://brapi.org/unsubscribe \nCheers \nPeter Selby',
+        text: 'Hello ' + req.body.name + '\nThank you for joining the BrAPI Community Mailing List. This list is the best way to stay up to date with the BrAPI community. Also, please consider joining the community Slack workspace and following us on Twitter/X.\nYou can Unsubscribe at any time by going to: https://brapi.org/unsubscribe\n\nCheers\nPeter Selby\nBrAPI Project Coordinator',
         'h:X-Mailgun-Variables': JSON.stringify({
-            "title": "Important: BrAPI Mailing List Update - Message 2 of 2",
-            "date": "August 19, 2022",
-            "author": "Peter Selby",
-            "article": "<p>We are changing to a new mailing server for the BrAPI mailing list. You should receive two messages today: (1) A message from the old server address \"<plant-breeding-api-bounces@mail2.sgn.cornell.edu>\" and (2) THIS message from the new server address \"announcements@mail.brapi.org\". </p><p>Congratulations! If you are reading this, then your address has been successfully migrated to the new system, and your spam filter has been configured correctly to allow messages from \"@mail.brapi.org\".</p><p>This new mail server should provide more stability and features, as well as making it easier for me to manage news posts and announcements. Moving to the community emails \"mail.brapi.org\" domain will hopefully future-proof the system, so even if the mail server needs to change again, the address will not. Thank you for your patience and cooperation as we make these adjustments. </p><p>If you do not want to be included on the BrAPI mailing list anymore, you can <a href=\"https://brapi.org/unsubscribe\">Unsubscribe</a> here: <a href=\"https://brapi.org/unsubscribe\">https://brapi.org/unsubscribe</a></p><p>Cheers<br>Peter Selby</p>"
+            "title": "Welcome to the BrAPI Mailing List",
+            "date": now.toDateString,
+            "author": null,
+            "article": "<p>Hello " + req.body.name + "</p><p>Thank you for joining the BrAPI Community Mailing List. This list is the best way to stay up to date with the BrAPI community. Also, please consider joining the community <a href=\"https://join.slack.com/t/plantbreedingapi/shared_invite/enQtNjA4NTA3OTI5NjUxLWE5ZmI0NDE0NGM1ODkxMjVmMDU1MGVjY2Q5M2QxNGNkYzMyODhkNDVmZjM0ZGI1YzEwYjEwNmY0MDM1YjllZDU\">Slack workspace</a> and following us on <a href=\"https://twitter.com/breedingapi\">Twitter/X</a>.</p><p>You can <a href=\"https://brapi.org/unsubscribe\">Unsubscribe</a> at any time by going to: <a href=\"https://brapi.org/unsubscribe\">https://brapi.org/unsubscribe</a></p><p>Cheers<br>Peter Selby<br>BrAPI Project Coordinator</p>"
         }),
         'h:List-Unsubscribe': 'http://brapi.org/unsubscribe',
         'h:Date': now.toUTCString()
     }
 
-    mg.messages.create('mail.brapi.org', emailData)
-    .then(msg => console.log(msg)) 
-    .catch(err => console.error(err));
-    fs.appendFile(process.env.MIALING_LIST_PATH, '+ ' + req.body.name + ' (' + req.body.org + ')<' + req.body.email + '>\n', function(err) {
+    mg.lists.members.createMember('announcement@mail.brapi.org', {
+        address: req.body.email,
+        name: req.body.name + ' (' + req.body.org + ')', // optional, modifiable on website
+        vars: { organization: req.body.org }, // optional, modifiable on website
+        subscribed: 'yes', // optional, modifiable on website
+        upsert: 'yes', // optional, choose yes to insert if not exist, or update it exist
+    })
+        .then(data => {
+            console.log(data);
+
+            mg.messages.create('mail.brapi.org', emailData)
+                .then(msg => console.log(msg))
+                .catch(err => console.error(err));
+        }) // logs response body
+        .catch(err => console.error(err)); // logs any error
+
+    fs.appendFile(process.env.MIALING_LIST_PATH, '+ ' + req.body.name + ' (' + req.body.org + ')<' + req.body.email + '>\n', function (err) {
         if (err) throw err;
         res.json({ "success": "true" })
     });
 });
 
-router.post('/mailingListUnsubscribe', function(req, res, next) {
-    fs.appendFile(process.env.MIALING_LIST_PATH, '- <' + req.body.email + '>\n', function(err) {
+router.post('/mailingListUnsubscribe', function (req, res, next) {
+    const now = new Date(Date.now());
+    var emailData = {
+        from: 'BrAPI  Mailing List <mail@mail.brapi.org>',
+        to: [req.body.email, 'mail@mail.brapi.org'],
+        subject: 'Unsubscribed from the BrAPI Mailing List',
+        template: 'announcement_template',
+        text: 'Hello ' + req.body.email + '\nYou have been unsubscribed from the BrAPI Community Mailing List. \nYou can resubscribe at any time by going to: https://brapi.org/contact\n\nCheers\nPeter Selby\nBrAPI Project Coordinator',
+        'h:X-Mailgun-Variables': JSON.stringify({
+            "title": "Unsubscribed from the BrAPI Mailing List",
+            "date": now.toDateString,
+            "author": null,
+            "article": '<p>Hello ' + req.body.email + '</p><p>You have been unsubscribed from the BrAPI Community Mailing List. You can resubscribe at any time by going to: <a href=\"https://brapi.org/contact\">https://brapi.org/contact</a></p><p>Cheers<br>Peter Selby<br>BrAPI Project Coordinator</p>'
+        }),
+        'h:List-Unsubscribe': 'http://brapi.org/unsubscribe',
+        'h:Date': now.toUTCString()
+    }
+
+    mg.lists.members.destroyMember('announcement@mail.brapi.org', req.body.email)
+        .then(data => {
+            console.log(data);
+            mg.messages.create('mail.brapi.org', emailData)
+                .then(msg => console.log(msg))
+                .catch(err => console.error(err));
+        }) // logs response body
+        .catch(err => console.error(err)); // logs any error
+
+    fs.appendFile(process.env.MIALING_LIST_PATH, '- <' + req.body.email + '>\n', function (err) {
         if (err) throw err;
         res.json({ "success": "true" })
     });
 });
 
-router.post('/newServerSubmit', function(req, res, next) {
+router.post('/newServerSubmit', function (req, res, next) {
     var newServerStr = "";
 
     if (req.body.existingOrg == "true") {
@@ -61,22 +100,94 @@ router.post('/newServerSubmit', function(req, res, next) {
         newServerStr += JSON.stringify(newOrg, null, 4)
     }
 
-    fs.appendFile(process.env.NEW_SERVER_LIST_PATH, newServerStr + '\n-------------\n', function(err) {
+    const now = new Date(Date.now());
+    var emailData = {
+        from: 'New Software <software@mail.brapi.org>',
+        to: ['software@mail.brapi.org', req.body.email],
+        template: 'announcement_template',
+        subject: 'New BrAPI software to review',
+        text: 'New BrAPI software to review\n\n\n' + newServerStr,
+        'h:X-Mailgun-Variables': JSON.stringify({
+            "title": 'New BrAPI software to review',
+            "date": null,
+            "author": null,
+            "article": newServerStr.replace(/\n/g, '<br>').replace(/  /g, '&nbsp;')
+        }),
+        'h:List-Unsubscribe': 'http://brapi.org/unsubscribe',
+        'h:Date': now.toUTCString()
+    }
+
+    mg.messages.create('mail.brapi.org', emailData)
+        .then(msg => console.log(msg))
+        .catch(err => console.error(err));
+
+    fs.appendFile(process.env.NEW_SERVER_LIST_PATH, newServerStr + '\n-------------\n', function (err) {
         if (err) throw err;
         res.json({ "success": "true" })
     });
 });
 
-router.post('/newSoftwareSubmit', function(req, res, next) {
+router.post('/newSoftwareSubmit', function (req, res, next) {
     var newServerStr = JSON.stringify(req.body, null, 4);
+    
+    const now = new Date(Date.now());
+    var emailData = {
+        from: 'New Software <software@mail.brapi.org>',
+        to: ['software@mail.brapi.org', req.body.email],
+        template: 'announcement_template',
+        subject: 'New BrAPI software to review',
+        text: 'New BrAPI software to review\n\n\n' + newServerStr,
+        'h:X-Mailgun-Variables': JSON.stringify({
+            "title": 'New BrAPI software to review',
+            "date": null,
+            "author": null,
+            "article": newServerStr.replace(/\n/g, '<br>').replace(/  /g, '&nbsp;')
+        }),
+        'h:List-Unsubscribe': 'http://brapi.org/unsubscribe',
+        'h:Date': now.toUTCString()
+    }
 
-    fs.appendFile(process.env.NEW_SERVER_LIST_PATH, newServerStr + '\n-------------\n', function(err) {
+    mg.messages.create('mail.brapi.org', emailData)
+        .then(msg => console.log(msg))
+        .catch(err => console.error(err));
+
+    fs.appendFile(process.env.NEW_SERVER_LIST_PATH, newServerStr + '\n-------------\n', function (err) {
         if (err) throw err;
         res.json({ "success": "true" })
     });
 });
 
-router.post('/testEndpoint', async function(req, res, next) {
+router.post('/announcement', function (req, res, next) {
+    const now = new Date(Date.now());
+    var emailData = {
+        from: 'BrAPI Announcements <announcement@mail.brapi.org>',
+        to: 'announcement@mail.brapi.org',
+        template: 'announcement_template',
+        subject: req.body.title,
+        text: req.body.text,
+        'h:X-Mailgun-Variables': JSON.stringify({
+            "title": req.body.title,
+            "date": req.body.date,
+            "author": req.body.author,
+            "article": req.body.article
+        }),
+        'h:List-Unsubscribe': 'http://brapi.org/unsubscribe',
+        'h:Date': now.toUTCString()
+    }
+
+    mg.messages.create('mail.brapi.org', emailData)
+        .then(msg => {
+            console.log(msg);
+            res.json({ "success": true, "message": msg });
+        })
+        .catch(err => {
+            console.error(err)
+            res.json({ "success": false, "error": err });
+        });
+
+});
+
+router.post('/testEndpoint', async function (req, res, next) {
     var server = req.body;
     var v1URL = server["server-v1-url"]
     var responseBody = { "v1Res": { "status": 0 }, "v2Res": { "status": 0 } };
@@ -84,7 +195,7 @@ router.post('/testEndpoint', async function(req, res, next) {
         //Production Only Code
         if (v1URL.includes('https://test-server.brapi.org'))
             v1URL = v1URL.replace('https://test-server.brapi.org', 'http://brapi-java-server-v1:8080')
-            // ----
+        // ----
         if (!v1URL.endsWith('/'))
             v1URL = v1URL + '/';
 
@@ -101,7 +212,7 @@ router.post('/testEndpoint', async function(req, res, next) {
         //Production Only Code
         if (v2URL.includes('https://test-server.brapi.org'))
             v2URL = v2URL.replace('https://test-server.brapi.org', 'http://brapi-java-server-v2:8080')
-            // ----
+        // ----
         if (!v2URL.endsWith('/'))
             v2URL = v2URL + '/';
 
@@ -159,7 +270,7 @@ function sendNotification(params) {
         text: 'Thank you for subscribing to the BrAPI Mailing List!'
     };
 
-    transporter.sendMail(mailOptions, function(error, info) {
+    transporter.sendMail(mailOptions, function (error, info) {
         if (error) {
             console.log(error);
         } else {
